1b99321bb93145a688be466dad4070cd60a6912c
k <k@k.com>
2023-11-04 01:50:14 -0700

    added copilot, some QOL keybinds, ChatGPT.nvim?

--- config.lua
+++ config.lua
@@ -10,8 +10,67 @@ doom.use_package("EdenEast/nightfox.nvim", "sainnhe/sonokai")
 doom.use_package({
   "ur4ltz/surround.nvim",
   config = function()
-    require("surround").setup({mappings_style = "surround"})
- end
+    require("surround").setup({ mappings_style = "surround" })
+  end,
+})
+
+-- Sane jk behaviour
+doom.use_keybind({
+  {
+    { "j", "gj" },
+    { "k", "gk" },
+  },
+})
+
+doom.use_keybind({
+  -- The `name` field will add the keybind to whichkey
+  {
+    "<leader>t",
+    name = "+tweak/toggle",
+    {
+      -- Bind to a vim command
+      { "w", "<cmd>set wrap!<CR>", name = "Toggle word wrap" },
+    },
+  },
+})
+
+doom.use_package({
+  "github/copilot.vim",
+  config = function()
+    vim.g.copilot_no_tab_map = true
+  end,
+})
+doom.use_keybind({
+  { "<Space>", "<Nop>" },
+  -- https://github.com/orgs/community/discussions/29817#discussioncomment-4217615
+  {
+    mode = "i",
+    {
+      {
+        options = { expr = true, script = true, replace_keycodes = false },
+        {
+          { "<C-g>",  'copilot#Accept("<CR>")' },
+          { "<C-CR>", 'copilot#Accept("<CR>")' },
+        },
+      },
+      { "<C-j>", "<Plug>(copilot-next)" },
+      { "<C-k>", "<Plug>(copilot-previous)" },
+      { "<C-o>", "<Plug>(copilot-dismiss)" },
+      { "<C-f>", "<Plug>(copilot-suggest)" },
+    },
+  },
+})
+
+doom.use_package({
+  "jackMort/ChatGPT.nvim",
+  config = function()
+    require("chatgpt").setup({})
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
 })
 
 -- ADDING A KEYBIND
