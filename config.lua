526666073ffeb185256c3e0f1467dbfe81a185d9
k <k@k.com>
2023-11-09 14:45:06 -0800

    trouble fixes attempt + suppress deprecated method alert

--- config.lua
+++ config.lua
@@ -14,6 +14,41 @@ doom.use_package({
   end,
 })
 
+-- Unpin trouble
+doom.features.trouble.packages["trouble.nvim"] = { "folke/trouble.nvim" }
+doom.features.trouble.configs["trouble.nvim"] = function()
+  require("trouble").setup({
+    severity = { vim.diagnostic.severity.ERROR },
+    action_keys = {
+      switch_severity = { "<c-w>" },
+    },
+    signs = {
+      error = "",
+      warning = "",
+      hint = "",
+      information = "",
+      other = "",
+    },
+  })
+end
+
+-- doom.features.trouble.settings = {
+--   fold_open = "-",
+--   fole_closed = "+",
+--   -- severity = { vim.diagnostic.severity.ERROR},
+--   severity = { "error"},
+--   action_keys = {
+--     switch_severity = { "<c-w>" },
+--   },
+--   signs = {
+--     error = "",
+--     warning = "",
+--     hint = "",
+--     information = "",
+--     other = "",
+--   },
+-- }
+
 -- Sane jk behaviour
 doom.use_keybind({
   {
@@ -108,3 +143,21 @@ doom.use_package({
 -- })
 
 -- vim: sw=2 sts=2 ts=2 expandtab
+
+-- PATCH: in order to address the message:
+-- vim.treesitter.query.get_query() is deprecated, use vim.treesitter.query.get() instead. :help deprecated
+--   This feature will be removed in Nvim version 0.10
+local orig_notify = vim.notify
+local filter_notify = function(text, level, opts)
+  -- more specific to this case
+  if
+      type(text) == "string"
+      and (string.find(text, "get_query", 1, true) or string.find(text, "get_node_text", 1, true))
+  then
+    -- for all deprecated and stack trace warnings
+    -- if type(text) == "string" and (string.find(text, ":help deprecated", 1, true) or string.find(text, "stack trace", 1, true)) then
+    return
+  end
+  orig_notify(text, level, opts)
+end
+vim.notify = filter_notify
